e2a234e11ded7e23f2ba1c4ab3345d1e323caaa5
Anthony Herbert <anthony@prettyprinted.com>
2022-04-24 19:09:13 +0700

    added return value to accept

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     #
     import socket
     s = socket.create_server(("localhost", 6379), reuse_port=True)
-    s.accept() # wait for client
+    c, addr = s.accept() # wait for client
 
     '''
     while True:
