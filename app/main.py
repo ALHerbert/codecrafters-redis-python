b76f7917b267609bc63188779e2e429042067205
Anthony Herbert <anthony@prettyprinted.com>
2022-04-24 19:29:11 +0700

    added threads to handle multiple connections

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from _thread import start_new_thread
+import threading
+
+client_lock = threading.Lock()
+
+def handle_client(c):
+    with c:
+        print(f"Connected by {addr}")
+        while True:
+            data = c.recv(1024)
+            print("data", data)
+            if not data:
+                client_lock.release()
+                break
+            c.send(b"+PONG\r\n")
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -6,16 +21,17 @@ def main():
     #
     import socket
     s = socket.create_server(("localhost", 6379), reuse_port=True)
-    c, addr = s.accept() # wait for client
 
-    with c:
-        print(f"Connected by {addr}")
-        while True:
-            data = c.recv(1024)
-            print("data", data)
-            if not data:
-                break
-            c.send(b"+PONG\r\n")
+    while True:
+        c, addr = s.accept() # wait for client
+
+        client_lock.acquire()
+        
+        start_new_thread(handle_client, (c,))
+
+    s.close()
+
+
 
 if __name__ == "__main__":
     main()
